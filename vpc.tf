9fa90654952f1d0bb94bae81fa14e64876286386
Jonas Axelshult <jonas.axelshult@gotamedia.se>
2021-02-23 12:37:18 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/jonasaxelshult-training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+  network_name = "jonas-network"
+  project-id = var.project
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
